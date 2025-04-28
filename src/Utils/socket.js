5ae57f53ffddfffb24a7989be6b77dd538f8f31f
Syed Faisal Hassan <faisalhassan77889@gmail.com>
2025-04-28 11:12:49 +0530

    some minor changes

--- src/Utils/socket.js
+++ src/Utils/socket.js
@@ -8,7 +8,31 @@ const initializeSocket = (server) =>{
     })
     io.on("connection",(socket)=>{
         //handle events
-        console.log("a user connected");
+        // console.log("a user connected");
+        socket.on("joinChat",({userId,targetUserId})=>{
+            // console.log(targetUserId);
+            const roomId = [userId,targetUserId].sort().join("_");
+            console.log("joined the room Id " + roomId);
+            socket.join(roomId);
+        });
+
+        socket.on("sendMessage",({firstName,userId,targetUserId,text})=>{
+            console.log(firstName);
+            console.log(userId,targetUserId);
+            console.log(text);
+            const roomId = [userId,targetUserId].sort().join("_");
+            io.to(roomId).emit("receivedMessage",{
+                firstName,
+                userId,
+                targetUserId,
+                text,
+            })
+        })
+
+        socket.on("disconnect",()=>{
+            console.log("disconnected");
+        });
+
     })
 }
 module.exports = { initializeSocket };
