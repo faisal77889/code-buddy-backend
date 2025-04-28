85f5db038f2a199391e56cf2621d6560c6fa10fa
Syed Faisal Hassan <faisalhassan77889@gmail.com>
2025-04-28 11:35:02 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const http = require('http');
-
-// const mongoose = require("mongoose");
 const connectDB = require("./config/database");
 const cookieParser = require('cookie-parser');
 const authRouter = require('./routes/auth');
@@ -11,10 +9,9 @@ const userRouter = require('./routes/user');
 const cors = require('cors');
 const { initializeSocket } = require("./Utils/socket");
 
-
-
-
 const app = express();
+const PORT = process.env.PORT || 7777; // PORT setup here
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
@@ -22,25 +19,22 @@ app.use(cors({
 
 app.use(cookieParser());
 app.use(express.json());
-app.use("/",authRouter);
-app.use("/",profileRouter);
-app.use("/",requestRouter);
-app.use("/",userRouter);
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
 
 const httpServer = http.createServer(app);
 
 initializeSocket(httpServer);
 
-
-
-
 connectDB()
     .then(() => {
-        console.log("connected to the database cluster succesfully");
-        httpServer.listen('7777', () => {
-            console.log("server is listening on port 7777");
-        })
+        console.log("connected to the database cluster successfully");
+        httpServer.listen(PORT, () => {
+            console.log(`server is listening on port ${PORT}`);
+        });
     })
     .catch((err) => {
-        console.log("There is an error while connecting to the database cluster");
-    })
+        console.error("There is an error while connecting to the database cluster:", err);
+    });
